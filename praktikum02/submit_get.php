6d2bbf015cc4447416e0980acaadf339441051ea
aftertowerr <146068611+aftertowerr@users.noreply.github.com>
2024-03-09 10:30:46 +0700

    praktikum02

---
+++ praktikum02/submit_get.php
@@ -0,0 +1,15 @@
+<?php
+  $nama = $_GET['nama'];
+  $matkul = $_GET['matkul'];
+  $nilai_uts = $_GET['nilai_uts'];
+  $nilai_uas = $_GET['nilai_uas'];
+  $nilai_tugas = $_GET['nilai_tugas'];
+
+  
+  echo "Nama: " . $nama . "<br>";
+  echo "Matkul: " . $matkul . "<br>";
+  echo "Nilai UTS: " . $nilai_uts . "<br>";
+  echo "Nilai UTS: " . $nilai_uas . "<br>";
+  echo "Nilai UTS: " . $nilai_tugas;
+
+?>
